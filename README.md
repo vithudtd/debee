1c2a618a60bdab4b5298b73844cf1f212a44e3bc
vithuanaz <vithuanaz1995@gmail.com>
2022-12-20 14:46:48 +0530

    Signed-off-by: vithuanaz <vithuanaz1995@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# debee
+#  Debee
+#  Database
+git remote add myorigin git@github.com:myname/oldrep.git 
+git remote set-url website git@github.com:myname/oldrep.git
---
+++ src/DebeeServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace Apptimus\Debee;
+
+use Apptimus\Debee\Console\Commands\TestVersion;
+use Illuminate\Support\ServiceProvider;
+
+class DebeeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                TestVersion::class
+            ]);
+        }
+    }
+
+    public function register()
+    {
+        # code...
+    }
+}
